803e30bfde106cf094581aabfc80ced2062c5ab7
Naveen CS <catchspider2002@gmail.com>
2024-08-31 20:48:06 -0400

    .

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -84,7 +84,7 @@
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
           >
-            <path d={Values.icons.twitter} />
+            <path d={Values.icons.x} />
           </svg></a
         >
         <a
